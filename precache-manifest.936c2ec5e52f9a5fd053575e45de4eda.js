00283a6c6fecec5867d6f3ed229b6055c8c1ba91
Peter Cardenas <petercardenas441@gmail.com>
2019-09-13 12:51:10 -0500

    Updates

---
+++ precache-manifest.936c2ec5e52f9a5fd053575e45de4eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea41ac45f7bdc6b6a3701d9d24c23e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57ea13ec7be574c935f6",
+    "url": "/static/css/main.f8dd8536.chunk.css"
+  },
+  {
+    "revision": "704f518e4a2ae3b0e930",
+    "url": "/static/js/2.428394aa.chunk.js"
+  },
+  {
+    "revision": "57ea13ec7be574c935f6",
+    "url": "/static/js/main.4f8cc550.chunk.js"
+  },
+  {
+    "revision": "fd9401c5a4cc0adeb619",
+    "url": "/static/js/runtime~main.a45aa8df.js"
+  }
+]);
