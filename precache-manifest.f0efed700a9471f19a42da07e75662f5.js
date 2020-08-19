10a25b265ec9d1be93138d0ada60b884df08d12e
root <root@pc-ubuntu.home>
2020-08-19 02:34:06 -0500

    added accordion and updated content

---
+++ precache-manifest.f0efed700a9471f19a42da07e75662f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c79251564d4cbc6b456f1076dca6dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25928a736fa7f4ed0ae5",
+    "url": "/static/css/main.f8dd8536.chunk.css"
+  },
+  {
+    "revision": "007de4b4d0696db94c0c",
+    "url": "/static/js/2.8a65f2ca.chunk.js"
+  },
+  {
+    "revision": "2a7ddd6924cd19423573c8c25a45983c",
+    "url": "/static/js/2.8a65f2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25928a736fa7f4ed0ae5",
+    "url": "/static/js/main.c4d4994f.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b16475a2a0d4552bf715da5488c151c.js"
+  "/precache-manifest.f0efed700a9471f19a42da07e75662f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
